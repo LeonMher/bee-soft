08e2276b498ad9e7949229bd9e946f059cfa7c7a
LeonMher <99.p3rc3n7@gmail.com>
2024-10-14 01:10:01 +0400

    make sure the button is aligned and is on the right spot

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import Hero from "./pages/Hero";
+import Navigation from "./pages/Navigation";
 
 function App() {
   return (
-    <div className="p-4">
+    <div className="p-4 flex flex-col gap-[30px]">
+      <Navigation />
       <Hero />
     </div>
   );
--- src/pages/Hero.tsx
+++ src/pages/Hero.tsx
@@ -1,19 +1,19 @@
-import Navigation from "./Navigation";
 import Button from "@mui/material/Button";
 
 const Hero = () => {
   return (
-    <div className="flex flex-col gap-[55px]">
-      <Navigation />
-      <div className="flex justify-center align-center">
-        <div className="flex justify-end align-end flex-col gap-[55px]">
+    <div className="flex flex-row justify-center gap-[55px]">
+      <div className="flex flex-col align-center gap-[35px]">
+        <div className="flex gap-[25px]">
           <p className="w-[312px] text-[32px] font-bold">
             Enhance your business with AI driven IT solutions
           </p>
         </div>
-      </div>
-      <div className="w-[160px] flex justify-center align-center">
-        <Button variant="contained">BOOK DEMO</Button>
+        <div className="w-[160px] flex">
+          <Button variant="contained" className="buttonColor">
+            Book a call
+          </Button>
+        </div>
       </div>
     </div>
   );
