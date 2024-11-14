72e7827f14481bb178080925a08deea862c2acab
Mher <99.p3rc3n7@gmail.com>
2024-11-14 22:19:40 +0400

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,28 @@ import "./App.css";
 import Hero from "./pages/Hero";
 import Navigation from "./pages/Navigation";
 import Slider from "./components/Slider";
+import Pic from "./assets/adamHakobyan.jpg";
 
 function App() {
+  const slides = [
+    {
+      avatar: Pic,
+      full_name: "Adam Hakobyan",
+      position: "Frontend Developer",
+      description: "I'm a frontend developer",
+    },
+    {
+      avatar: Pic,
+      full_name: "Sam Smith",
+      position: "Backend Developer",
+      description: "I'm a backend engineer",
+    },
+  ];
   return (
     <div>
       <Navigation />
       <Hero />
-      <Slider />
+      <Slider slides={slides} />
     </div>
   );
 }
--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,59 +1,64 @@
 import { useState } from "react";
-import { Cards } from "./Cards";
-import AdamHakobyanImage from "../assets/adamHakobyan.jpg";
-import SamSmithImage from "../assets/samSmith.jpg";
-// TODO: import arrow icons later
-// import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
-// import { RxDotFilled } from 'react-icons/rx';
 
-function Slider() {
-  const slides = [
-    {
-      url: AdamHakobyanImage,
-      full_name: "Sam Smith",
-      position: "CEO",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
-    },
-    {
-      url: SamSmithImage,
-    },
-  ];
+type CardProps = {
+  slides: {
+    avatar: string;
+    full_name: string;
+    position: string;
+    description?: string;
+  }[];
+};
 
+function Slider({ slides }: CardProps) {
   const [currentIndex, setCurrentIndex] = useState(0);
 
-  const prevSlide = () => {
-    const isFirstSlide = currentIndex === 0;
-    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
-    setCurrentIndex(newIndex);
-  };
-
   const nextSlide = () => {
-    const isLastSlide = currentIndex === slides.length - 1;
-    const newIndex = isLastSlide ? 0 : currentIndex + 1;
-    setCurrentIndex(newIndex);
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % slides.length);
   };
 
-  //   const goToSlide = (slideIndex: number) => {
-  //     setCurrentIndex(slideIndex);
-  //   };
+  const prevSlide = () => {
+    setCurrentIndex(
+      (prevIndex) => (prevIndex - 1 + slides.length) % slides.length
+    );
+  };
 
   return (
-    <div className="max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative group">
-      <Cards
-        avatar={slides[currentIndex].url}
-        full_name={slides[currentIndex].full_name}
-        position={slides[currentIndex].position}
-        description={slides[currentIndex].description}
-      />
-      {/* Left Arrow */}
-      <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
-        <span onClick={prevSlide}>left</span>
+    <div className="relative w-full max-w-lg mx-auto">
+      <div className="overflow-hidden">
+        <div
+          className="flex transition-transform duration-500 ease-in-out"
+          style={{
+            transform: `translateX(-${currentIndex * 100}%)`,
+          }}
+        >
+          {slides.map((slide, index) => (
+            <div className="w-full flex-shrink-0" key={index}>
+              <div className="w-300px">
+                <img src={slide.avatar} alt="" className="w-300px" />
+              </div>
+              <div>
+                <h3>{slide.full_name}</h3>
+                <p>{slide.position}</p>
+                <p>{slide.description}</p>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
-      {/* Right Arrow */}
-      <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
-        <span onClick={nextSlide}>right</span>
-      </div>
+
+      <button
+        onClick={prevSlide}
+        className="absolute top-1/2 left-0 transform -translate-y-1/2 bg-white text-black p-2 rounded-full shadow-md"
+      >
+        &#10094;
+      </button>
+
+      <button
+        onClick={nextSlide}
+        className="absolute top-1/2 right-0 transform -translate-y-1/2 bg-white text-black p-2 rounded-full shadow-md"
+      >
+        &#10095;
+      </button>
     </div>
   );
 }
